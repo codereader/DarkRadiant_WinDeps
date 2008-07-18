fb73ca5da00e7e6b8c50e250d23ff2f7a281408c
codereader <greebo@angua.at>
2008-07-18 06:58:24 +0000

    Updated win32 libxlm2 sources to 2.6.32.
    git-svn-id: svn://192.168.56.2/trunk/w32deps@3669 80d3f8d2-1b0e-0410-a445-ca0696aa8c50

--- libxml2/config.h
+++ libxml2/config.h
@@ -23,6 +23,10 @@
 
 #include <libxml/xmlversion.h>
 
+#ifndef ICONV_CONST
+#define ICONV_CONST const
+#endif
+
 #ifdef NEED_SOCKETS
 #include <wsockcompat.h>
 #endif
@@ -92,12 +96,9 @@ static int isnan (double d) {
 #if defined(_MSC_VER)
 #define mkdir(p,m) _mkdir(p)
 #define snprintf _snprintf
-
-// greebo: Do not redefine vsnprintf in Visual C++ 2008 (_MSC_VER == 1500)
-#if (_MSC_VER < 1500)
+#if _MSC_VER < 1500
 #define vsnprintf(b,c,f,a) _vsnprintf(b,c,f,a)
 #endif
-
 #elif defined(__MINGW32__)
 #define mkdir(p,m) _mkdir(p)
 #endif
