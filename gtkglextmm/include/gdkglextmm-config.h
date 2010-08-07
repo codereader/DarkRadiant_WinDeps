790313016240b768ab86c6035a7dcc332ea75d19
codereader <greebo@angua.at>
2010-08-07 16:39:48 +0000

    Removed windows.h inclusion from gdkglextmm-config.h. This seems to be required only when compiling the DLL.
    git-svn-id: svn://192.168.56.2/branches/w32deps_gtkmm@5843 80d3f8d2-1b0e-0410-a445-ca0696aa8c50

--- gtkglextmm/include/gdkglextmm-config.h
+++ gtkglextmm/include/gdkglextmm-config.h
@@ -3,10 +3,5 @@
 
 // greebo: this file seems to be necessary to compile gdkglextmm 
 // and it was missing in the gtkglextmm package, so here it is
-// The inclusion below is suggested as found here: http://mirex.mypage.sk/FILES/gtkglextmm-vs9.txt
-
-#define WIN32_LEAN_AND_MEAN 1
-#include <windows.h>
-#undef max 
 
 #endif 
