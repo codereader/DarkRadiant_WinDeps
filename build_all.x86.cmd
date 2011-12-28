0781083e0a0a4804600272f2aa5e835bf18fd5da
codereader <greebo@angua.at>
2011-12-28 04:50:54 +0000

    Dependency tweaks and new build scripts.
    git-svn-id: svn://192.168.56.2/trunk/winbuild@6908 80d3f8d2-1b0e-0410-a445-ca0696aa8c50

--- build_all.x86.cmd
+++ build_all.x86.cmd
@@ -1,20 +1,2 @@
-msbuild libsigc++\MSVC_Net2010\libsigc++2.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild libsigc++\MSVC_Net2010\libsigc++2.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild glibmm\MSVC_Net2010\glibmm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild glibmm\MSVC_Net2010\glibmm.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild cairomm\MSVC_Net2010\cairomm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild cairomm\MSVC_Net2010\cairomm.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild pangomm\MSVC_Net2010\pangomm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild pangomm\MSVC_Net2010\pangomm.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild gtkmm\MSVC_Net2010\gtkmm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild gtkmm\MSVC_Net2010\gtkmm.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild gtksourceviewmm\MSVC_Net2010\gtksourceviewmm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild gtksourceviewmm\MSVC_Net2010\gtksourceviewmm.sln /p:configuration=release /p:platform=Win32  /m
-
-msbuild gtkglextmm\MSVC_Net2010\gtkglextmm.sln /p:configuration=debug /p:platform=Win32  /m
-msbuild gtkglextmm\MSVC_Net2010\gtkglextmm.sln /p:configuration=release /p:platform=Win32  /m
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=debug /p:platform=Win32 /m /t:rebuild
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=release /p:platform=Win32 /m /t:rebuild
