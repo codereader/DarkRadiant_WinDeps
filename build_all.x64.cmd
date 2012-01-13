f6ec8a4856ea0dbae29af5e20d9ea282f3819c4d
codereader <greebo@angua.at>
2012-01-13 16:38:50 +0000

    Update build scripts.
    git-svn-id: svn://192.168.56.2/trunk/winbuild@6939 80d3f8d2-1b0e-0410-a445-ca0696aa8c50

--- build_all.x64.cmd
+++ build_all.x64.cmd
@@ -1,2 +1,2 @@
-msbuild msvc2010\darkradiant_windeps.sln /p:configuration=debug /p:platform=x64 /m /t:rebuild
-msbuild msvc2010\darkradiant_windeps.sln /p:configuration=release /p:platform=x64 /m /t:rebuild
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=debug /p:platform=x64
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=release /p:platform=x64
--- build_all.x86.cmd
+++ build_all.x86.cmd
@@ -1,2 +1,2 @@
-msbuild msvc2010\darkradiant_windeps.sln /p:configuration=debug /p:platform=Win32 /m /t:rebuild
-msbuild msvc2010\darkradiant_windeps.sln /p:configuration=release /p:platform=Win32 /m /t:rebuild
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=debug /p:platform=Win32 
+msbuild msvc2010\darkradiant_windeps.sln /p:configuration=release /p:platform=Win32 
