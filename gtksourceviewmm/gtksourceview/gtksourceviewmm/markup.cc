c0b8fa57ff4f4528f8c55e4ee9144b43fac239cd
codereader <greebo@angua.at>
2012-09-23 05:26:31 +0000

    Add gtksourceviewmm 3.2.0 sources and add export prefixes to many gtksourceview funcions, as we're lacking a .def file.
    git-svn-id: svn://192.168.56.2/branches/winbuild_gtk3@7131 80d3f8d2-1b0e-0410-a445-ca0696aa8c50

---
+++ gtksourceviewmm/gtksourceview/gtksourceviewmm/markup.cc
@@ -0,0 +1,75 @@
+/* markup.cc
+ *
+ * Copyright (C) 2009, 2010 Krzesimir Nowak
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <gtksourceviewmm/markup.h>
+
+
+namespace Gsv
+{
+
+Markup::Markup()
+:
+  markup_ ("")
+{}
+
+Markup::Markup(const Glib::ustring& markup)
+:
+  markup_ (markup)
+{}
+
+Markup::Markup(const char* markup)
+:
+  markup_ ((markup) ? markup : "")
+{}
+
+Markup::~Markup()
+{}
+
+Markup::Markup(const Markup& other)
+:
+  markup_ (other.markup_)
+{}
+
+Markup& Markup::operator=(const Markup& other)
+{
+  markup_ = other.markup_;
+  return *this;
+}
+
+Markup::operator const void*() const
+{
+  return !(markup_.empty()) ? GINT_TO_POINTER(1) : 0;
+}
+
+bool Markup::equal(const Markup& rhs) const
+{
+  return (markup_.raw() == rhs.markup_.raw());
+}
+
+Glib::ustring Markup::get_string() const
+{
+  return markup_;
+}
+
+const char* Markup::get_c_str() const
+{
+  return markup_.c_str();
+}
+
+} // namespace Gsv
--- gtksourceviewmm/gtksourceview/gtksourceviewmm/wrap_init.cc
+++ gtksourceviewmm/gtksourceview/gtksourceviewmm/wrap_init.cc
@@ -12,15 +12,29 @@
 
 // #include the widget headers so that we can call the get_type() static methods:
 
-#include "sourcestyle.h"
-#include "sourcestylescheme.h"
-#include "sourcelanguage.h"
-#include "sourcelanguagemanager.h"
-#include "sourceview.h"
-#include "sourcebuffer.h"
-#include "sourceiter.h"
-#include "sourcemark.h"
-#include "sourcestyleschememanager.h"
+#include "buffer.h"
+#include "completion.h"
+#include "completionactivation.h"
+#include "completioncontext.h"
+#include "completioninfo.h"
+#include "completionitem.h"
+#include "completionproposal.h"
+#include "completionprovider.h"
+#include "completionwords.h"
+#include "gutter.h"
+#include "gutterrenderer.h"
+#include "gutterrendererpixbuf.h"
+#include "gutterrenderertext.h"
+#include "language.h"
+#include "languagemanager.h"
+#include "mark.h"
+#include "markattributes.h"
+#include "printcompositor.h"
+#include "style.h"
+#include "stylescheme.h"
+#include "styleschememanager.h"
+#include "undomanager.h"
+#include "view.h"
 
 extern "C"
 {
@@ -28,54 +42,104 @@ extern "C"
 //Declarations of the *_get_type() functions:
 
 GType gtk_source_buffer_get_type(void);
+GType gtk_source_completion_get_type(void);
+GType gtk_source_completion_context_get_type(void);
+GType gtk_source_completion_info_get_type(void);
+GType gtk_source_completion_item_get_type(void);
+GType gtk_source_completion_words_get_type(void);
+GType gtk_source_gutter_get_type(void);
+GType gtk_source_gutter_renderer_get_type(void);
+GType gtk_source_gutter_renderer_pixbuf_get_type(void);
+GType gtk_source_gutter_renderer_text_get_type(void);
 GType gtk_source_language_get_type(void);
 GType gtk_source_language_manager_get_type(void);
 GType gtk_source_mark_get_type(void);
+GType gtk_source_mark_attributes_get_type(void);
+GType gtk_source_print_compositor_get_type(void);
 GType gtk_source_style_get_type(void);
+GType gtk_source_style_scheme_get_type(void);
 GType gtk_source_style_scheme_manager_get_type(void);
 GType gtk_source_view_get_type(void);
 
 //Declarations of the *_error_quark() functions:
 
+GQuark gtk_source_completion_error_quark(void);
 } // extern "C"
 
 
 //Declarations of the *_Class::wrap_new() methods, instead of including all the private headers:
 
-namespace gtksourceview {  class SourceBuffer_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceLanguage_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceLanguageManager_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceMark_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceStyle_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceStyleSchemeManager_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
-namespace gtksourceview {  class SourceView_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Buffer_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Completion_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class CompletionContext_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class CompletionInfo_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class CompletionItem_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class CompletionWords_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Gutter_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class GutterRenderer_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class GutterRendererPixbuf_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class GutterRendererText_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Language_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class LanguageManager_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Mark_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class MarkAttributes_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class PrintCompositor_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class Style_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class StyleScheme_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class StyleSchemeManager_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
+namespace Gsv {  class View_Class { public: static Glib::ObjectBase* wrap_new(GObject*); };  }
 
-namespace gtksourceview { 
+namespace Gsv { 
 
 void wrap_init()
 {
   // Register Error domains:
+  Glib::Error::register_domain(gtk_source_completion_error_quark(), &::Gsv::SourceCompletionError::throw_func);
 
 // Map gtypes to gtkmm wrapper-creation functions:
-  Glib::wrap_register(gtk_source_buffer_get_type(), &gtksourceview::SourceBuffer_Class::wrap_new);
-  Glib::wrap_register(gtk_source_language_get_type(), &gtksourceview::SourceLanguage_Class::wrap_new);
-  Glib::wrap_register(gtk_source_language_manager_get_type(), &gtksourceview::SourceLanguageManager_Class::wrap_new);
-  Glib::wrap_register(gtk_source_mark_get_type(), &gtksourceview::SourceMark_Class::wrap_new);
-  Glib::wrap_register(gtk_source_style_get_type(), &gtksourceview::SourceStyle_Class::wrap_new);
-  Glib::wrap_register(gtk_source_style_scheme_manager_get_type(), &gtksourceview::SourceStyleSchemeManager_Class::wrap_new);
-  Glib::wrap_register(gtk_source_view_get_type(), &gtksourceview::SourceView_Class::wrap_new);
+  Glib::wrap_register(gtk_source_buffer_get_type(), &Gsv::Buffer_Class::wrap_new);
+  Glib::wrap_register(gtk_source_completion_get_type(), &Gsv::Completion_Class::wrap_new);
+  Glib::wrap_register(gtk_source_completion_context_get_type(), &Gsv::CompletionContext_Class::wrap_new);
+  Glib::wrap_register(gtk_source_completion_info_get_type(), &Gsv::CompletionInfo_Class::wrap_new);
+  Glib::wrap_register(gtk_source_completion_item_get_type(), &Gsv::CompletionItem_Class::wrap_new);
+  Glib::wrap_register(gtk_source_completion_words_get_type(), &Gsv::CompletionWords_Class::wrap_new);
+  Glib::wrap_register(gtk_source_gutter_get_type(), &Gsv::Gutter_Class::wrap_new);
+  Glib::wrap_register(gtk_source_gutter_renderer_get_type(), &Gsv::GutterRenderer_Class::wrap_new);
+  Glib::wrap_register(gtk_source_gutter_renderer_pixbuf_get_type(), &Gsv::GutterRendererPixbuf_Class::wrap_new);
+  Glib::wrap_register(gtk_source_gutter_renderer_text_get_type(), &Gsv::GutterRendererText_Class::wrap_new);
+  Glib::wrap_register(gtk_source_language_get_type(), &Gsv::Language_Class::wrap_new);
+  Glib::wrap_register(gtk_source_language_manager_get_type(), &Gsv::LanguageManager_Class::wrap_new);
+  Glib::wrap_register(gtk_source_mark_get_type(), &Gsv::Mark_Class::wrap_new);
+  Glib::wrap_register(gtk_source_mark_attributes_get_type(), &Gsv::MarkAttributes_Class::wrap_new);
+  Glib::wrap_register(gtk_source_print_compositor_get_type(), &Gsv::PrintCompositor_Class::wrap_new);
+  Glib::wrap_register(gtk_source_style_get_type(), &Gsv::Style_Class::wrap_new);
+  Glib::wrap_register(gtk_source_style_scheme_get_type(), &Gsv::StyleScheme_Class::wrap_new);
+  Glib::wrap_register(gtk_source_style_scheme_manager_get_type(), &Gsv::StyleSchemeManager_Class::wrap_new);
+  Glib::wrap_register(gtk_source_view_get_type(), &Gsv::View_Class::wrap_new);
 
   // Register the gtkmm gtypes:
-  gtksourceview::SourceBuffer::get_type();
-  gtksourceview::SourceLanguage::get_type();
-  gtksourceview::SourceLanguageManager::get_type();
-  gtksourceview::SourceMark::get_type();
-  gtksourceview::SourceStyle::get_type();
-  gtksourceview::SourceStyleSchemeManager::get_type();
-  gtksourceview::SourceView::get_type();
+  Gsv::Buffer::get_type();
+  Gsv::Completion::get_type();
+  Gsv::CompletionContext::get_type();
+  Gsv::CompletionInfo::get_type();
+  Gsv::CompletionItem::get_type();
+  Gsv::CompletionWords::get_type();
+  Gsv::Gutter::get_type();
+  Gsv::GutterRenderer::get_type();
+  Gsv::GutterRendererPixbuf::get_type();
+  Gsv::GutterRendererText::get_type();
+  Gsv::Language::get_type();
+  Gsv::LanguageManager::get_type();
+  Gsv::Mark::get_type();
+  Gsv::MarkAttributes::get_type();
+  Gsv::PrintCompositor::get_type();
+  Gsv::Style::get_type();
+  Gsv::StyleScheme::get_type();
+  Gsv::StyleSchemeManager::get_type();
+  Gsv::View::get_type();
 
 } // wrap_init()
 
-} //gtksourceview
+} //Gsv
 
 
