1b83ba84543b9cdb6ba9658118cf6a363d379000
codereader <greebo@angua.at>
2021-10-12 06:10:07 +0200

    Add missing python headers

--- winbuild/compress_windeps.cmd
+++ winbuild/compress_windeps.cmd
@@ -1,2 +1,2 @@
 cd ..\
-"c:\Program Files\7-Zip\7z.exe" a -r -mx9 -x!*.ilk -x!*.exp -x!*.pdb windeps.7z windeps/*
+"c:\Program Files\7-Zip\7z.exe" a -r -mmt2 -mx9 -x!*.ilk -x!*.exp -x!*.pdb windeps.7z windeps/*
